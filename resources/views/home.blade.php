f179b3ecf3ec390f5ff4b6c3563b318d14aa22c2
Flopiras <Flopiras@yahoo.it>
2023-08-22 16:26:05 +0200

    add movies cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,26 @@
 
 @section('main')
 
+<div class="container mt-4">
+    <div class="row row-cols-4">
+        @foreach($movies as $movie)
+            <div class="col">
+
+                {{-- cards --}}
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                        <p class="card-text"><strong>Nationality : </strong> {{ $movie->nationality }}</p>
+                        <p class="card-text"><strong>Date : </strong> {{ $movie->date }}</p>
+                        <p class="card-text"><strong>Vote : </strong> {{ $movie->vote }}</p>
+                        <a href="#" class="card-link">Movie link</a>
+                    </div>
+              </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 
 @endsection
