0635c88bd8727a22383d61ef8ff00801a042cfbd
Flopiras <Flopiras@yahoo.it>
2023-08-22 16:37:16 +0200

    add navbar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,22 @@
 <header>
-    <div class="container mt-4 text-center">
-        <h1>Laravel Movies</h1>
-    </div>
+        {{-- navbar --}}
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+              <a class="navbar-brand" href="{{ route('home') }}">{{env('APP_NAME') }}</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    {{-- home --}}
+                  <li class="nav-item">
+                    <a class="nav-link @if(Route::is('home')) active @endif" aria-current="page" href="{{ route('home') }}">Home</a>
+                  </li>
+                  {{-- about --}}
+                  <li class="nav-item">
+                    <a class="nav-link @if(Route::is('about')) active @endif" aria-current="page" href="{{ url('/about') }}">Chi Siamo</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
 </header>
